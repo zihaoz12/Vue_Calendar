c771c5be5a6fe5f84d5ce34b51e19438feab5c05
zihaoz12 <zzhaa12@gmail.com>
2019-10-17 16:13:04 -0700

    add Delete and Update Events

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -71,20 +71,26 @@
               :color="selectedEvent.color"
               dark
             >
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details"></span>
+              <form v-if="currentlyEditing !== selectedEvent.id">
+                  {{selectedEvent.details}}
+              </form>
+              <form v-else>
+                <textarea-autosize
+                  v-model="selectedEvent.details"
+                  type="text"
+                  style="width: 100%"
+                  :min-height="100"
+                  placeholder="add note"
+                >
+                </textarea-autosize>
+              </form>
             </v-card-text>
             <v-card-actions>
               <v-btn
@@ -92,7 +98,21 @@
                 color="secondary"
                 @click="selectedOpen = false"
               >
-                Cancel
+                Close
+              </v-btn>
+              <v-btn
+                text
+                v-if="currentlyEditing !== selectedEvent.id"
+                @click.prevent = "editEvent(selectedEvent)"
+              >
+                Edit
+              </v-btn>
+              <v-btn
+                text
+                v-else
+                @click.prevent = "updateEvent(selectedEvent)"
+              >
+                Save
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -179,38 +199,53 @@ export default {
             });
             this.events = events
         },
+        async updateEvent(ev){
+          await db.collection('calEvent').doc(this.currentlyEditing).update({
+            details: ev.details
+          });
+          this.selectedOpen = false;
+          this.currentlyEditing = null;
+        },
+        async deleteEvent(ev){
+          await db.collection('calEvent').doc(ev).delete();
+          this.selectedOpen = false;
+          this.getEvents();
+        },
         viewDay ({ date }) {
           this.focus = date
           this.type = 'day'
-      },
+        },
         getEventColor (event) {
           return event.color
-      },
+        },
         setToday () {
           this.focus = this.today
-      },
+        },
         prev () {
           this.$refs.calendar.prev()
-      },
+        },
         next () {
           this.$refs.calendar.next()
-      },
+        },
+        editEvent(ev){
+          this.currentlyEditing = ev.id
+        },
         showEvent ({ nativeEvent, event }) {
-          const open = () => {
+            const open = () => {
             this.selectedEvent = event
             this.selectedElement = nativeEvent.target
             setTimeout(() => this.selectedOpen = true, 10)
-        }
+          }
 
           if (this.selectedOpen) {
             this.selectedOpen = false
             setTimeout(open, 10)
-        }   else {
+          }   else {
             open()
-        }
+          }
 
           nativeEvent.stopPropagation()
-      },
+        },
       updateRange ({ start, end }) {
         // You could load events from an outside source (like database) now that we have the start and end dates on the calendar
         this.start = start
